2222b18a51ae281cd12b717300432bb60564d2a6
Xenith <fing2024@outlook.com>
2026-08-26 00:14:10 +0800

    revert formula to v0.0.4 (v0.0.5 release withdrawn)

--- Formula/openpanda.rb
+++ Formula/openpanda.rb
@@ -2,27 +2,27 @@ class Openpanda < Formula
   desc "Personal adaptive node-based distributed assistant (agent-of-agents)"
   homepage "https://github.com/Xustalis/OpenPanda"
   license "MIT"
-  version "0.0.5"
+  version "0.0.4"
 
   depends_on "python@3.12"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Xustalis/OpenPanda/releases/download/v#{version}/panda-#{version}-darwin-arm64.tar.gz"
-      sha256 "dc23fb0c6d3afde02c7e2df4d3684268b32c3987f40ff4a3e6268c76c3fcfc3b"
+      sha256 "5a79dd1ac22fceb6d1844837a504bb528b307bcab05ecde13b5e9aa528352d26"
     else
       url "https://github.com/Xustalis/OpenPanda/releases/download/v#{version}/panda-#{version}-darwin-amd64.tar.gz"
-      sha256 "992516f0ced85f1bb84bae8e859b9eae9a2597f2cca2eb5367182699edeb8be3"
+      sha256 "5e33aae02b274c9644757d416e2fd42259956d87a453044d21163a2a82ccfe0c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Xustalis/OpenPanda/releases/download/v#{version}/panda-#{version}-linux-arm64.tar.gz"
-      sha256 "10268c49470e15b72ddc3f72a56cc98b235d8d0030b34f9eba6ad60f5c79c081"
+      sha256 "f531ab493ee4d4b5cf95a81892242463e30e56e04055b5123f1f0382a2143b5a"
     else
       url "https://github.com/Xustalis/OpenPanda/releases/download/v#{version}/panda-#{version}-linux-amd64.tar.gz"
-      sha256 "3316ce26c1beec6811afa901a238d05e4a8a576f5c8a0d72904df14f2e3250d4"
+      sha256 "0017d6dccdeaa4fc984e3755e82cae85655787157211b2c4a79f21bf64a6d37e"
     end
   end
 
